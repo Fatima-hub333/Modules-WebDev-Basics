a71b6949705de73c88daf3c6d355a139bd74becb
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-09 11:05:13 +0500

    update index.html & index.js

--- Module-03 (Interactive- Websites)/passenger-count-app/index.js
+++ Module-03 (Interactive- Websites)/passenger-count-app/index.js
@@ -4,12 +4,23 @@ let count = 5
 count = count + 1
 console.log(count)
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
+// function increment() {
+//   console.log('button is clicked')
+// }
 
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+// Setting up the race
+function logLapTime() {
+  let totalTime = lap1 + lap2 + lap3
+  console.log(totalTime)
+}
+
+logLapTime()
+
