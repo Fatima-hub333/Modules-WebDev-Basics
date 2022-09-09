3b8b1261ea5c7c0b2079bd7aaacf40dc8f6df02a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-09 05:31:21 +0500

    mathematical operations

--- Module-03 (Interactive- Websites)/passenger-count-app/index.js
+++ Module-03 (Interactive- Websites)/passenger-count-app/index.js
@@ -1,4 +1,11 @@
 // document.getElementById('count-el').innerHTML = 5
 
-let count = 0
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
 console.log(count)
+
+let myAge = 35
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
