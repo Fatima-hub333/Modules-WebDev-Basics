46038609410876a5a7f9e5de2835ab2c4e56a57f
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-09 14:39:18 +0500

    function that increments

--- Module-03 (Interactive- Websites)/passenger-count-app/index.js
+++ Module-03 (Interactive- Websites)/passenger-count-app/index.js
@@ -1,26 +1,13 @@
 // document.getElementById('count-el').innerHTML = 5
 
-let count = 5
-count = count + 1
-console.log(count)
-
-// initialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
-
-// function increment() {
-//   console.log('button is clicked')
-// }
-
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
-// Setting up the race
-function logLapTime() {
-  let totalTime = lap1 + lap2 + lap3
-  console.log(totalTime)
+let lapsCompleted = 0
+function incrementLap() {
+  lapsCompleted = lapsCompleted + 1
 }
 
-logLapTime()
+incrementLap()
+incrementLap()
+incrementLap()
+
+console.log(lapsCompleted)
 
