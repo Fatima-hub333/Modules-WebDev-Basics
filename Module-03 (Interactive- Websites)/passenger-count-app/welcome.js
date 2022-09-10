4b14c921b49d4853719bcfa44651ad5014bd5fcc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-10 12:51:27 +0500

    Practice JS challenges

--- Module-03 (Interactive- Websites)/passenger-count-app/welcome.js
+++ Module-03 (Interactive- Websites)/passenger-count-app/welcome.js
@@ -1,9 +1,44 @@
-let welcomeEl = document.getElementById('welcome-el')
+// let welcomeEl = document.getElementById('welcome-el')
 
-let name = 'Fatima'
-let greeting = 'Welcome Back '
-welcomeEl.innerText = greeting + name
+// let name = 'Fatima'
+// let greeting = 'Welcome Back '
+// welcomeEl.innerText = greeting + name
 
-console.log(welcomeEl)
+// console.log(welcomeEl)
 
-welcomeEl.innerText += '👋'
+// welcomeEl.innerText += '👋'
+
+// let firstName = 'Fatima '
+// let lastName = 'Zahir'
+
+// let fullName = firstName + lastName
+// console.log(fullName)
+
+// let name = 'Linda'
+// let greeting = 'Hi there'
+
+// function myGreetings() {
+//   console.log(greeting + ', ' + name + '!')
+// }
+// myGreetings()
+
+// let myPoints = 3
+
+// function add3Points() {
+//   myPoints += 3
+// }
+
+// function remove1Point() {
+//   myPoints -= 1
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+// console.log(myPoints)
+
+let errorParagraph = document.getElementById('error')
+function purchase() {
+  errorParagraph.textContent = "Something went wrong, please try again"
+}
