1cda6ec5d4c827773b403ae1ba59ec57f776641d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 11:11:51 +0500

    create first card

---
+++ BlackJack/script.js
@@ -0,0 +1,9 @@
+// 1. Create two variables, firstCard and secondCard.
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = 6
+let secondCard = 9
+let sum = firstCard + secondCard
+console.log(sum)
