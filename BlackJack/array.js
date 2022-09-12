3ee306178ee97584a49215c5706c28f04bedd7c8
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 14:47:29 +0500

    Solving card problem with an array

---
+++ BlackJack/array.js
@@ -0,0 +1,47 @@
+// let featuredPosts = [
+//   "Check out my Netflix clone",
+//   "Here's the code for my project",
+//   "I've  just relaunched my portfolio"
+// ]
+
+// console.log(featuredPosts.length)
+// let experience = [
+//   'CEO at Scrimba',
+//   'Front-end developer at Toptal',
+//   'People Count for Norstat'
+// ]
+
+// console.log(experience[1])
+// console.log(experience[2])
+// console.log(experience[0])
+
+// Array with multiple data types
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+let fatimaZahir = [
+  'Fatima Zahir',
+  25,
+  true
+]
+console.log(fatimaZahir)
+
+// Add & remove from array
+
+let cards = []
+cards.push(6)
+console.log(cards)
+
+let messages = [
+  "Hey, how's it going?",
+  "I'm great, thank you! How about you?",
+  "All good. Been working on my portfolio lately."
+]
+
+let m = newMessage = "Same here!"
+
+messages.push(newMessage)
+console.log(messages)
+
+messages.pop()
+console.log(messages)
--- BlackJack/script.js
+++ BlackJack/script.js
@@ -4,6 +4,7 @@ let cardsEl =document.getElementById('cards-el')
 
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasblackJack = false
 let isAlive = true
@@ -14,8 +15,8 @@ function startGame() {
 }
 
 function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + ' ' + cards[1]
   sumEl.textContent = "Sum: " + sum
-  cardsEl.textContent = "Cards: " + firstCard + ' ' + secondCard
   if (sum <= 20) {
   message= "Do you want to draw a new card?"
 } else if (sum === 21) {
