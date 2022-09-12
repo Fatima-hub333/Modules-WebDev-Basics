439ec8128806147ceefa6625d37273b68fa13449
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 16:26:24 +0500

    Random number, Math.random , Math.floor practice

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -1,44 +1,52 @@
-let messageEl = document.getElementById('message-el')
-let sumEl = document.getElementById('sum-el')
-let cardsEl =document.getElementById('cards-el')
+// let messageEl = document.getElementById('message-el')
+// let sumEl = document.getElementById('sum-el')
+// let cardsEl =document.getElementById('cards-el')
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
-let hasblackJack = false
-let isAlive = true
-let message = ''
+// let firstCard = getRandomCard()
+// let secondCard = getRandomCard()
+// let cards = [firstCard, secondCard]
+// let sum = firstCard + secondCard
+// let hasblackJack = false
+// let isAlive = true
+// let message = ''
 
-function getRandomCard() {
-  return 5
+// function getRandomCard() {
+//   return 5
+// }
+
+// function startGame() {
+//   renderGame()
+// }
+
+// function renderGame() {
+//   for (let i = 0; i < cards.length; i += 1) {
+//     cardsEl.textContent = "Cards: " + cards + "  "
+//   }
+
+//   sumEl.textContent = "Sum: " + sum
+//   if (sum <= 20) {
+//   message= "Do you want to draw a new card?"
+// } else if (sum === 21) {
+//   message = "Wohoo! You've got Blackjack!"
+//   hasblackJack = true
+// } else {
+//   message = "You're out of the game!"
+//   isAlive = false
+// }
+// messageEl.textContent = message
+// }
+
+// function newCard() {
+//   let card = getRandomCard()
+//   sum += card
+//   cards.push(card)
+//   renderGame()
+// }
+
+
+function rollDice() {
+  let randomNumber = Math.floor(Math.random() * 6) + 1
+  return randomNumber
 }
 
-function startGame() {
-  renderGame()
-}
-
-function renderGame() {
-  for (let i = 0; i < cards.length; i += 1) {
-    cardsEl.textContent = "Cards: " + cards + "  "
-  }
-
-  sumEl.textContent = "Sum: " + sum
-  if (sum <= 20) {
-  message= "Do you want to draw a new card?"
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack!"
-  hasblackJack = true
-} else {
-  message = "You're out of the game!"
-  isAlive = false
-}
-messageEl.textContent = message
-}
-
-function newCard() {
-  let card = getRandomCard()
-  sum += card
-  cards.push(card)
-  renderGame()
-}
+console.log(rollDice())
