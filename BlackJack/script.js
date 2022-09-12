5f381b0043760be6c8ebe9715a2b81a37ff696dd
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 11:23:44 +0500

    if-else conditions practice

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -1,9 +1,28 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
+// let firstCard = 15
+// let secondCard = 9
+// let sum = firstCard + secondCard
+// console.log(sum)
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+// if (sum < 21) {
+//   console.log('Do you want to draw an another card?')
+// } else if (sum === 21) {
+//   console.log('Whooo! You won the game  🥳')
+// } else{
+//   console.log('Oh no! You are out of the game1! 😭')
+// }
 
-let firstCard = 6
-let secondCard = 9
-let sum = firstCard + secondCard
-console.log(sum)
+// Check if the person is elegible for a birthday card from the King! (100)
+
+let age = 100
+
+// if less than 100    -> "Not elegible"
+// else if exactly 100 -> "Here is your birthday card from the King!"
+// else                -> "Not elegible, you have already gotten one"
+
+if (age < 100) {
+  console.log("Not elegible")
+} else if (age === 100) {
+  console.log("Here is your birthday card from the King!")
+} else (
+  console.log("Not elegible, you have already gotten one")
+)
