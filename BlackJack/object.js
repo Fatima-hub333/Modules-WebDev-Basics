fd73ee5322db9c9243231917cc8feb8db64cb278
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 20:29:26 +0500

    create your first object

--- BlackJack/object.js
+++ BlackJack/object.js
@@ -1,11 +1,21 @@
-let course = {
-  title: "Learn CSS Grid for free",
-  lessons: 16,
-  creator: "Fatima Zahir",
-  length: 63,
-  level: 2,
-  isFree: true,
-  tags: ["html", "css"]
+// let course = {
+//   title: "Learn CSS Grid for free",
+//   lessons: 16,
+//   creator: "Fatima Zahir",
+//   length: 63,
+//   level: 2,
+//   isFree: true,
+//   tags: ["html", "css"]
+// }
+
+// console.log(course.tags)
+
+let castle = {
+  title: "Live like a king in my castle",
+  price: 190,
+  isSuperHost: true,
+  features: ["4 guests", "1 bedroom", "2 beds"]
 }
 
-console.log(course.tags)
+console.log(castle.price)
+console.log(castle.isSuperHost)
