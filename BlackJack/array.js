93f30890bf00b915beb59f4a15d338e479be52fd
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 19:56:42 +0500

    Update array.js

--- BlackJack/array.js
+++ BlackJack/array.js
@@ -19,29 +19,40 @@
 // Create an array that describes yourself. Use the three primitive data types you've learned
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
 
-let fatimaZahir = [
-  'Fatima Zahir',
-  25,
-  true
-]
-console.log(fatimaZahir)
+// let fatimaZahir = [
+//   'Fatima Zahir',
+//   25,
+//   true
+// ]
+// console.log(fatimaZahir)
 
 // Add & remove from array
 
-let cards = []
-cards.push(6)
-console.log(cards)
+// let cards = []
+// cards.push(6)
+// console.log(cards)
 
-let messages = [
-  "Hey, how's it going?",
-  "I'm great, thank you! How about you?",
-  "All good. Been working on my portfolio lately."
-]
+// let messages = [
+//   "Hey, how's it going?",
+//   "I'm great, thank you! How about you?",
+//   "All good. Been working on my portfolio lately."
+// ]
 
-let m = newMessage = "Same here!"
+// let m = newMessage = "Same here!"
 
-messages.push(newMessage)
-console.log(messages)
+// messages.push(newMessage)
+// console.log(messages)
 
-messages.pop()
-console.log(messages)
+// messages.pop()
+// console.log(messages)
+
+let hasCompletedCourse = true
+let givesCertificate = true
+
+if (hasCompletedCourse === true && givesCertificate === true) {
+  generateCertificate()
+}
+
+function generateCertificate() {
+  console.log("Generating certificate...")
+}
