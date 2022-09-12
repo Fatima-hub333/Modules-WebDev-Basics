850588c049efdaee380aca1d69978c313fb261fc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 20:23:03 +0500

    intro to objects

---
+++ BlackJack/object.js
@@ -0,0 +1,11 @@
+let course = {
+  title: "Learn CSS Grid for free",
+  lessons: 16,
+  creator: "Fatima Zahir",
+  length: 63,
+  level: 2,
+  isFree: true,
+  tags: ["html", "css"]
+}
+
+console.log(course.tags)
