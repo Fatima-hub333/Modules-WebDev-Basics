ddc8292d440d988e1c3fd828e1040c1deaf065f9
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 12:02:45 +0500

    isAlive variable

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -1,7 +1,8 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 15
 let sum = firstCard + secondCard
 let hasblackJack = false
+let isAlive = true
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂")
@@ -10,6 +11,7 @@ if (sum <= 20) {
   hasblackJack = true
 } else {
   console.log("You're out of the game! 😭")
+  isAlive = false
 }
 
-console.log(hasblackJack)
+console.log(isAlive)
