b622ce9d303a5466a9194b763233257b4f193085
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 13:25:13 +0500

    Display the sum

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -1,4 +1,6 @@
 let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
+
 let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
@@ -7,6 +9,7 @@ let isAlive = true
 let message = ''
 
 function startGame() {
+  sumEl.textContent = "Sum: " + sum
   if (sum <= 20) {
   message= "Do you want to draw a new card?"
 } else if (sum === 21) {
