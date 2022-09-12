40f1424d2175f894ef736a612efdd96124e2e138
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 19:42:35 +0500

    Assign values in the startGame function

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -2,17 +2,15 @@ let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl =document.getElementById('cards-el')
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasblackJack = false
-let isAlive = true
+let isAlive = false
 let message = ''
 
+console.log(cards)
+
 function getRandomCard() {
-  // if 1     -> return 11
-  // if 11-13 -> return 10
   let randomNumer = Math.floor( Math.random()*13 ) + 1
   if (randomNumer > 10) {
     return 10
@@ -24,6 +22,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
   renderGame()
 }
 
