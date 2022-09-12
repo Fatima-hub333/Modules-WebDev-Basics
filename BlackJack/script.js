9454167fca7283e5f8e038676096419cc2702f18
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-12 11:58:03 +0500

    Practice if-else statment

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -1,28 +1,12 @@
-// let firstCard = 15
-// let secondCard = 9
-// let sum = firstCard + secondCard
-// console.log(sum)
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard + 10
+console.log(sum)
 
-// if (sum < 21) {
-//   console.log('Do you want to draw an another card?')
-// } else if (sum === 21) {
-//   console.log('Whooo! You won the game  🥳')
-// } else{
-//   console.log('Oh no! You are out of the game1! 😭')
-// }
-
-// Check if the person is elegible for a birthday card from the King! (100)
-
-let age = 100
-
-// if less than 100    -> "Not elegible"
-// else if exactly 100 -> "Here is your birthday card from the King!"
-// else                -> "Not elegible, you have already gotten one"
-
-if (age < 100) {
-  console.log("Not elegible")
-} else if (age === 100) {
-  console.log("Here is your birthday card from the King!")
-} else (
-  console.log("Not elegible, you have already gotten one")
-)
+if (sum <= 20) {
+  console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+  console.log("Wohoo! You've got Blackjack! 🥳")
+} else {
+  console.log("You're out of the game! 😭")
+}
