29f61b81bebbbb13c5432c03606a5ac57e67c3ba
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 12:07:21 +0500

    Refactor to addeventlistener

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -1,3 +1,4 @@
-function saveLead() {
-  console.log("Button clicked")
-}
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked from addeventlistener")
+})
