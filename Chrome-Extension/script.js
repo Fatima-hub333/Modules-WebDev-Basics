671c5b3398ab3acc91fac3582ef57d496294087b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 12:37:39 +0500

    create the unordered list

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -1,6 +1,8 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
   console.log(myLeads)
