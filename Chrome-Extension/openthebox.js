edb32c165a1845c88f03b0fcba04efe100e6810b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 14:23:30 +0500

    Update styling & practice innerHTML

--- Chrome-Extension/openthebox.js
+++ Chrome-Extension/openthebox.js
@@ -1,16 +1,24 @@
-let box = document.getElementById("box")
-box.addEventListener("click", function () {
-  console.log("I want to open the box")
-})
+// let box = document.getElementById("box")
+// box.addEventListener("click", function () {
+//   console.log("I want to open the box")
+// })
 
-const baseprice = 520
-const discount = 120
-let shippingCost = 12
-let shippingTime = "5-12 days"
+// const baseprice = 520
+// const discount = 120
+// let shippingCost = 12
+// let shippingTime = "5-12 days"
 
-shippingCost = 15
-shippingTime = "7-14 days"
+// shippingCost = 15
+// shippingTime = "7-14 days"
 
-const fullprice = baseprice - discount + shippingCost
+// const fullprice = baseprice - discount + shippingCost
   
-console.log("Total cost: " + fullprice + ". it will arrive in " + shippingTime)
+// console.log("Total cost: " + fullprice + ". it will arrive in " + shippingTime)
+
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'> Button!</button>"
+
+function buy() {
+  container.innerHTML += "<p>Thank You for buying!</p>"
+}
