ea24a56dfc0d1c7d29a77452053db8e1bfb5e9f3
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 12:23:08 +0500

    when to use const and let

--- Chrome-Extension/openthebox.js
+++ Chrome-Extension/openthebox.js
@@ -2,3 +2,15 @@ let box = document.getElementById("box")
 box.addEventListener("click", function () {
   console.log("I want to open the box")
 })
+
+const baseprice = 520
+const discount = 120
+let shippingCost = 12
+let shippingTime = "5-12 days"
+
+shippingCost = 15
+shippingTime = "7-14 days"
+
+const fullprice = baseprice - discount + shippingCost
+  
+console.log("Total cost: " + fullprice + ". it will arrive in " + shippingTime)
