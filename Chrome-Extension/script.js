f4f82e1306212d4f6b4b98fa87478259683b063c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 12:25:21 +0500

    push to the myLeads array

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -2,5 +2,6 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked from addeventlistener")
+  myLeads.push("www.awesomelead.com")
+  console.log(myLeads)
 })
