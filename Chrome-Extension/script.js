53a8175b13cdee0b1e73c35e24990be3cc9d2c11
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 12:30:42 +0500

    use a for loop to logout leads

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -1,7 +1,11 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
   console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i += 1) {
+  console.log(myLeads[i])
+}
