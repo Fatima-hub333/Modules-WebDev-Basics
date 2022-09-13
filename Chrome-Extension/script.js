5153236ecea1750a424ae0cad049e0f19d59700c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 18:20:29 +0500

    storing arrays in localstorage

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -3,10 +3,15 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const olEl = document.getElementById("ol-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
   inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
+  console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {
