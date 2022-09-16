365eeeb8e87a608174f1cb2cd54c9ff72982f1d6
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-16 20:36:32 +0500

    displaying the dice number

---
+++ JS-essential-concepts/dicegame/script.js
@@ -0,0 +1,42 @@
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+rollBtn.addEventListener("click", () => {
+  const randomNumber = Math.floor(Math.random() * 6) +1
+
+  if (player1Turn) {
+    player1Score += randomNumber
+    player1Scoreboard.textContent = player1Score
+    player1Dice.textContent = randomNumber
+    player1Dice.classList.remove("active")
+    player2Dice.classList.add("active")
+    message.textContent = "Player 2 Turn"
+  } else {
+    player2Score += randomNumber
+    player2Scoreboard.textContent = player2Score
+    player2Dice.textContent = randomNumber
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    message.textContent = "Player 1 Turn"
+  }
+
+  if (player1Score >= 20) {
+    message.textContent = "Player 1 has won! 🥳"
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+  }else if (player2Score >= 20) {
+    message.textContent = "Player 2 has won! 🥳"
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+  }
+  player1Turn = !player1Turn
+})
