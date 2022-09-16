cc02106ffe914ce7267eb168167d40c1ed9effb0
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-16 12:46:04 +0500

    looping over all our names

--- JS-essential-concepts/searchbar/script.js
+++ JS-essential-concepts/searchbar/script.js
@@ -2,5 +2,9 @@ let searchInput = document.getElementById("searchInput")
 searchInput.addEventListener('keyup', () => {
   let searchQuery = event.target.value.toLowerCase()
   let allNamesDOMCollection = document.getElementsByClassName("name")
-  console.log(allNamesDOMCollection[0])
+  
+  for (let counter = 0; counter < allNamesDOMCollection.length; counter ++) {
+    const currentName = allNamesDOMCollection[counter].textContent.toLocaleLowerCase()
+    console.log(currentName)
+  }
 })
