9df3fb6b848911c6aab18cf80ccfadc1628bca1a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-21 23:40:57 +0500

    render the characters

---
+++ Render-characters/index.js
@@ -0,0 +1,16 @@
+document.getElementById('hero').innerHTML = `
+   <div class="character-card">
+      <h4 class="name"> Wizard </h4>
+      <img class="avatar" src="images/wizard.png"/>
+      <p class="health">health: <b> 60 </b></p>
+      <div class="dice-container"><div class="dice"> 6 </div></div>
+    </div>  
+`
+document.getElementById('monster').innerHTML = `
+  <div class="character-card">
+    <h4 class="name"> Orc </h4>
+    <img class="avatar" src="images/orc.png"/>
+    <p class="health">health: <b> 10 </b></p>
+    <div class="dice-container"><div class="dice"> 4 </div></div>
+  </div>
+`
