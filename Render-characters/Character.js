11b90eb7ea57d493f594486dedaa18d7a9ea0b87
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 14:26:06 +0500

    render dice values

--- Render-characters/Character.js
+++ Render-characters/Character.js
@@ -4,25 +4,26 @@ function Character(data) {
   Object.assign(this, data)
   
   this.diceArray = getDicePlaceholderHtml(this.diceCount)
-
-  this.getDiceHtml = function (diceCount) {
-    return getDiceRollArray(diceCount).map(function(num) {
-    return `<div class="dice">${num}</div>`
-  }).join('')
-  }
-
+  
+  this.getDiceHtml = function() {
+    this.currentDiceScore = getDiceRollArray(this.diceCount)
+    this.diceArray = this.currentDiceScore.map(function(num){
+        return `<div class="dice">${num}</div>`
+    }).join('')
+  } 
+  
   this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount } = this
-    const diceHtml = this.getDiceHtml(diceCount)
-
-    return `
-    <div class="character-card">
-      <h4 class="name"> ${name} </h4>
-      <img class="avatar" src="${avatar}"/>
-      <p class="health">health: <b> ${health} </b></p>
-      <div class="dice-container">${this.diceArray} </div>
-    </div>
-  `
-  }
+    const { elementId, name, avatar, health, diceCount } = this;      
+      return `
+      <div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}" />
+        <div class="health">health: <b> ${health} </b></div>
+        <div class="dice-container">
+            ${this.diceArray}
+        </div>
+      </div>`
+  }  
 }
+
 export default Character
--- Render-characters/index.js
+++ Render-characters/index.js
@@ -1,16 +1,18 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
-function attack() {
+function attack(){
+  wizard.getDiceHtml()
+  orc.getDiceHtml()
   render()
 }
 
 function render() {
-  document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
-  document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+  document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+  document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
 
-document.getElementById('attack-button').addEventListener('click', attack)
+document.getElementById("attack-button").addEventListener('click', attack)
 
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
