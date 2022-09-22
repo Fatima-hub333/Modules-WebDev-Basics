0b48e0c362d4aa757b663320350a74a639e235eb
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 15:52:43 +0500

    ternary operator challenge

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -1,28 +1,50 @@
-import characterData from './data.js'
-import Character from './Character.js'
+// import characterData from './data.js'
+// import Character from './Character.js'
 
-function attack(){
-  wizard.getDiceHtml()
-  orc.getDiceHtml()
-  wizard.takeDamage(orc.currentDiceScore)
-  orc.takeDamage(wizard.currentDiceScore)
-  render()
-  if (wizard.dead || orc.dead) {
-    endGame()
-  }
-}
+// function attack(){
+//   wizard.getDiceHtml()
+//   orc.getDiceHtml()
+//   wizard.takeDamage(orc.currentDiceScore)
+//   orc.takeDamage(wizard.currentDiceScore)
+//   render()
+//   if (wizard.dead || orc.dead) {
+//     endGame()
+//   }
+// }
 
-function endGame() {
-  console.log('The game is over')
-}
+// function endGame() {
+//   console.log('The game is over')
+// }
 
-function render() {
-  document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-  document.getElementById('monster').innerHTML = orc.getCharacterHtml();
-}
+// function render() {
+//   document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+//   document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+// }
 
-document.getElementById("attack-button").addEventListener('click', attack)
+// document.getElementById("attack-button").addEventListener('click', attack)
 
-const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
-render()
+// const wizard = new Character(characterData.hero)
+// const orc = new Character(characterData.monster)
+// render()
+
+
+const playerGuess = 3
+const correctAnswer = 6
+
+/*
+Challenge 
+1. Refactor the if else statement to use a ternary operator.
+*/
+
+// let message = '' 
+// if(playerGuess === correctAnswer){
+//     message = 'Correct!'
+// }
+// else{
+//     message = 'Wrong!'
+// }
+
+const message = playerGuess < correctAnswer ? 'Too Low'
+  : playerGuess > correctAnswer ? 'Too High' 
+  : 'Exactly right'
+console.log(message)
