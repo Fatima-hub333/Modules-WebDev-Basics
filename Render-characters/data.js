40ca574c68beb181c68122b687b4d49b52c6a725
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 16:06:57 +0500

    endGame()

--- Render-characters/data.js
+++ Render-characters/data.js
@@ -2,15 +2,15 @@ const characterData = {
   hero: {
   name:'Wizard' ,
   avatar:'images/wizard.png' ,
-  health:'60',
-    diceCount: 3,
+  health:60,
+  diceCount: 3,
   currentDiceScore: []
 },
   monster: {
   name:'Orc' ,
   avatar:'images/orc.png' ,
-  health: '10',
-    diceCount: 1,
+  health: 10,
+  diceCount: 1,
   currentDiceScore: []
 }
 }
