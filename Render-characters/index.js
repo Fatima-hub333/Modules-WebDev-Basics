e3b7f48754b641ab6c8632db36cc2fabca850d1c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 09:45:45 +0500

    fix bug in loop

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -3,7 +3,7 @@ const hero = {
   name:'Wizard' ,
   avatar:'images/wizard.png' ,
   health:'60' ,
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
   diceCount: 3
 }
 
@@ -12,7 +12,7 @@ const monster = {
   name:'Orc' ,
   avatar:'images/orc.png' ,
   health: '10',
-  diceRoll: 4,
+  diceRoll: [2],
   diceCount: 1
 }
 
@@ -21,7 +21,7 @@ function renderCharacter(data) {
 
   let diceHtml = ''
   for (let i = 0; i < diceCount; i += 1) {
-    diceHtml += `<div class="dice">6</div>`
+    diceHtml += `<div class="dice">${diceRoll[i]}</div>`
   }
 
 
