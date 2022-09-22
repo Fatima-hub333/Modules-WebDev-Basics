5f5b7c0fbeb0c4792b55442b7df02cfd64cb715e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 13:56:09 +0500

    make the attack button working

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -4,6 +4,12 @@ import Character from './Character.js'
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 
+function attack() {
+  console.log('Attack button clicked')
+}
+
+document.getElementById('attack-button').addEventListener('click', attack)
+
 function render() {
   document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
   document.getElementById('monster').innerHTML = orc.getCharacterHtml()
