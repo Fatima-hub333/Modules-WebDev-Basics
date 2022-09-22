92b01eae9cfb33a51d7db6e01a2e219b839e84a3
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 23:01:43 +0500

    calculate percentage health remaining

--- Render-characters/Character.js
+++ Render-characters/Character.js
@@ -1,48 +1,56 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
 
-function Character(data) {
-    Object.assign(this, data)
-
-    this.diceArray = getDicePlaceholderHtml(this.diceCount)
-    
-/*
-CHALLENGE
-1. In the getDiceHtml method, there is an anonymous function 
-being used as an inline callback function. Replace it with an 
-arrow function.
-2. Do the same for the anonymous function in the takeDamage 
-method.
-3. Try to end up with the least amount of code possible!
+/*Challenge
+1. Create a property called maxHealth INSIDE Character that 
+stores the maximum health a character can have.
+2. Create an arrow function called getPercentage OUTSIDE 
+Character which takes two parameters, remainingHealth and 
+maximumHealth. getPercentage should return the % of 
+maximumHealth that is remaining.
+3. To test, call getPercentage from within the takeDamage method
+and log out the result. 
+**hint.md for help!!**
 */
 
-    this.getDiceHtml = function () {
-        this.currentDiceScore = getDiceRollArray(this.diceCount);
-        this.diceArray = this.currentDiceScore.map((num)=> 
-            `<div class="dice">${num}</div>`).join("")
-    }
-    
-    this.takeDamage = function(attackScoreArray){  
-        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
-        this.health -= totalAttackScore
-        if (this.health <= 0){
-            this.dead = true  
-            this.health = 0
-        } 
-    }
-    
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;
+const getPercentage = (remainingHealth, maximumHealth) => 
+(100 * remainingHealth) / maximumHealth
 
-        return `
-            <div class="character-card">
-                <h4 class="name"> ${name} </h4>
-                <img class="avatar" src="${avatar}" />
-                <div class="health">health: <b> ${health} </b></div>
-                <div class="dice-container">
-                    ${this.diceArray}
-                </div>
-            </div>`
-    }
+
+function Character(data) {
+Object.assign(this, data)
+this.diceArray = getDicePlaceholderHtml(this.diceCount)
+
+this.maxHealth = this.health
+
+this.getDiceHtml = function () {
+  this.currentDiceScore = getDiceRollArray(this.diceCount);
+  this.diceArray = this.currentDiceScore.map((num) =>
+      `<div class="dice">${num}</div>`).join("")
+}
+
+this.takeDamage = function(attackScoreArray){  
+  const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
+  this.health -= totalAttackScore
+  if (this.health <= 0){
+      this.dead = true  
+      this.health = 0
+  }
+  console.log(getPercentage(this.health, this.maxHealth)) 
+}
+
+this.getCharacterHtml = function () {
+  const { elementId, name, avatar, health, diceCount } = this;
+
+  return `
+      <div class="character-card">
+          <h4 class="name"> ${name} </h4>
+          <img class="avatar" src="${avatar}" />
+          <div class="health">health: <b> ${health} </b></div>
+          <div class="dice-container">
+              ${this.diceArray}
+          </div>
+      </div>`
+}
 }
 
 export default Character
--- Render-characters/index.js
+++ Render-characters/index.js
@@ -1,38 +1,38 @@
-// import characterData from './data.js'
-// import Character from './Character.js'
+import characterData from './data.js'
+import Character from './Character.js'
 
-// function attack(){
-//   wizard.getDiceHtml()
-//   orc.getDiceHtml()
-//   wizard.takeDamage(orc.currentDiceScore)
-//   orc.takeDamage(wizard.currentDiceScore)
-//   render()
-//   if (wizard.dead || orc.dead) {
-//     endGame()
-//   }
-// }
+function attack(){
+  wizard.getDiceHtml()
+  orc.getDiceHtml()
+  wizard.takeDamage(orc.currentDiceScore)
+  orc.takeDamage(wizard.currentDiceScore)
+  render()
+  if (wizard.dead || orc.dead) {
+    endGame()
+  }
+}
 
-// function endGame() {
-//   const endMessage = wizard.health === 0 && orc.health === 0 ? "No victors - all creatures are dead"
-//     : wizard.health > 0 ? "The Wizard Wins"
-//       : "The Orc is Victorious"
-//   const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-//   document.body.innerHTML = `
-//     <div class="end-game">
-//         <h2>Game Over</h2>
-//         <h3>${endMessage}</h3>
-//         <p class="end-emoji">${endEmoji}</p>
-//     </div>
-//   `
-// }
+function endGame() {
+  const endMessage = wizard.health === 0 && orc.health === 0 ? "No victors - all creatures are dead"
+    : wizard.health > 0 ? "The Wizard Wins"
+      : "The Orc is Victorious"
+  const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+  document.body.innerHTML = `
+    <div class="end-game">
+        <h2>Game Over</h2>
+        <h3>${endMessage}</h3>
+        <p class="end-emoji">${endEmoji}</p>
+    </div>
+  `
+}
 
-// function render() {
-//   document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-//   document.getElementById('monster').innerHTML = orc.getCharacterHtml();
-// }
+function render() {
+  document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+  document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+}
 
-// document.getElementById("attack-button").addEventListener('click', attack)
+document.getElementById("attack-button").addEventListener('click', attack)
 
-// const wizard = new Character(characterData.hero)
-// const orc = new Character(characterData.monster)
-// render()
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+render()
