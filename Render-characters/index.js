d8e7b6df82a4931794a7a987de9e029efd52728e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 23:42:55 +0500

    setTimeout challenge

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -1,67 +1,74 @@
-import characterData from './data.js'
-import Character from './Character.js'
+// import characterData from './data.js'
+// import Character from './Character.js'
 
-let monstersArray = ["orc", "demon", "goblin"]
+// let monstersArray = ["orc", "demon", "goblin"]
 
-function getNewMonster() {
-    const nextMonsterData = characterData[monstersArray.shift()]
-    return nextMonsterData ? new Character(nextMonsterData) : {}
-}
+// function getNewMonster() {
+//     const nextMonsterData = characterData[monstersArray.shift()]
+//     return nextMonsterData ? new Character(nextMonsterData) : {}
+// }
 
-/*
-Challenge
-1. Change the attack function so that when a monster dies, 
-the next monster replaces it. If there are no more monsters,
-call endGame(). 
-2. Make sure that endGame() still gets called if the wizard
-is killed.
-*/
+// /*
+// Challenge
+// 1. Change the attack function so that when a monster dies,
+// the next monster replaces it. If there are no more monsters,
+// call endGame().
+// 2. Make sure that endGame() still gets called if the wizard
+// is killed.
+// */
 
-function attack() {
-    wizard.getDiceHtml()
-    monster.getDiceHtml()
-    wizard.takeDamage(monster.currentDiceScore)
-    monster.takeDamage(wizard.currentDiceScore)
-    render()
+// function attack() {
+//     wizard.getDiceHtml()
+//     monster.getDiceHtml()
+//     wizard.takeDamage(monster.currentDiceScore)
+//     monster.takeDamage(wizard.currentDiceScore)
+//     render()
     
-        /*change the code below this line*/
-    if(wizard.dead){
-        endGame()
-    }
-    else if(monster.dead){
-        if(monstersArray.length > 0){
-            monster = getNewMonster()
-            render()
-        }
-        else{
-            endGame()
-        }
-    }
-}
+//         /*change the code below this line*/
+//     if(wizard.dead){
+//         endGame()
+//     }
+//     else if(monster.dead){
+//         if(monstersArray.length > 0){
+//             monster = getNewMonster()
+//             render()
+//         }
+//         else{
+//             endGame()
+//         }
+//     }
+// }
 
-function endGame() {
-    const endMessage = wizard.health === 0 && monster.health === 0 ?
-        "No victors - all creatures are dead" :
-        wizard.health > 0 ? "The Wizard Wins" :
-            "The Orc is Victorious"
+// function endGame() {
+//     const endMessage = wizard.health === 0 && monster.health === 0 ?
+//         "No victors - all creatures are dead" :
+//         wizard.health > 0 ? "The Wizard Wins" :
+//             "The Orc is Victorious"
 
-    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-    document.body.innerHTML = `
-        <div class="end-game">
-            <h2>Game Over</h2> 
-            <h3>${endMessage}</h3>
-            <p class="end-emoji">${endEmoji}</p>
-        </div>
-        `
-}
+//     const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+//     document.body.innerHTML = `
+//         <div class="end-game">
+//             <h2>Game Over</h2>
+//             <h3>${endMessage}</h3>
+//             <p class="end-emoji">${endEmoji}</p>
+//         </div>
+//         `
+// }
 
-document.getElementById("attack-button").addEventListener('click', attack)
+// document.getElementById("attack-button").addEventListener('click', attack)
 
-function render() {
-    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
-    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
-}
+// function render() {
+//     document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+//     document.getElementById('monster').innerHTML = monster.getCharacterHtml()
+// }
 
-const wizard = new Character(characterData.hero)
-let monster = getNewMonster()
-render()
+// const wizard = new Character(characterData.hero)
+// let monster = getNewMonster()
+// render()
+
+console.log(`Once upon a time, a beautiful princess met a handsome prince.`) //1
+setTimeout(() => console.log(`In the end, they lived happily ever after.`), 1500)//5
+console.log(`The princess's wicked stepmother put a curse on them before they could marry.`)//2
+setTimeout(() =>console.log(`They got married on a beautiful summer's day.`), 1000)//4
+console.log(`The prince found a friendly wizard to lift the curse.`)//3
+
