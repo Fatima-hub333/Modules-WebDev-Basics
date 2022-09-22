45b792ee7c88f45e0a2f2cf5fd75354e73dfd9ce
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 16:24:36 +0500

    inline arrow function() challenge

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -37,20 +37,15 @@
 // const orc = new Character(characterData.monster)
 // render()
 
+const distanceTraveledMiles = [267, 345, 234, 190, 299]
+
+const distanceTraveledKm = distanceTraveledMiles.map(distance => Math.round(distance * 1.6))
+
+console.log(distanceTraveledKm)
+
 /*
 Challenge
-1. Refactor this function so it only warns drivers 
-   who are going over the speed limit.
-2. The function now needs to take in two parameters. 
-   The first is the speed limit, the second is the 
-   driver's actual speed.
+1. Refactor this .map method so the inline function is
+   an arrow function. 
+2. Write the least amount of code possible.
 */
-
-const speedWarning = (speedLimit, speed) => {
-  if (speed > speedLimit) {
-    return`You are going at ${speed} mph!`
-  }
-  
-}
-
-console.log(speedWarning(30, 40))
