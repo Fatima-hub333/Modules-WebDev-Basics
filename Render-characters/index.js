d474bb8fae41a0e7687a2f8764636fbb4dc4673b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 09:40:28 +0500

    render multiple dice

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -3,27 +3,35 @@ const hero = {
   name:'Wizard' ,
   avatar:'images/wizard.png' ,
   health:'60' ,
-  diceRoll:6
+  diceRoll: 6,
+  diceCount: 3
 }
-
-// 
 
 const monster = {
   elementId:'monster' ,
   name:'Orc' ,
   avatar:'images/orc.png' ,
   health: '10',
-  diceRoll: 4
+  diceRoll: 4,
+  diceCount: 1
 }
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data
+
+  let diceHtml = ''
+  for (let i = 0; i < diceCount; i += 1) {
+    diceHtml += `<div class="dice">6</div>`
+  }
+
+
+
   document.getElementById(elementId).innerHTML = `
     <div class="character-card">
       <h4 class="name"> ${name} </h4>
       <img class="avatar" src="${avatar}"/>
       <p class="health">health: <b> ${health} </b></p>
-      <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+      <div class="dice-container">${diceHtml} </div>
     </div>
   `
 }
