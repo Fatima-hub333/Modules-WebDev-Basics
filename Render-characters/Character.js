070cdfdd4dd2c143528863b313c91e7de84bb49e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 23:12:32 +0500

    Displaying the health bar

--- Render-characters/Character.js
+++ Render-characters/Character.js
@@ -1,60 +1,72 @@
-import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
+  import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
 
-/*Challenge
-1. Create a property called maxHealth INSIDE Character that 
-stores the maximum health a character can have.
-2. Create an arrow function called getPercentage OUTSIDE 
-Character which takes two parameters, remainingHealth and 
-maximumHealth. getPercentage should return the % of 
-maximumHealth that is remaining.
-3. To test, call getPercentage from within the takeDamage method
-and log out the result. 
-**hint.md for help!!**
-*/
+  const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth
 
-const getPercentage = (remainingHealth, maximumHealth) => 
-(100 * remainingHealth) / maximumHealth
+  function Character(data) {
+  Object.assign(this, data)
+  this.maxHealth = this.health
 
+  this.diceArray = getDicePlaceholderHtml(this.diceCount)
 
-function Character(data) {
-Object.assign(this, data)
-this.diceArray = getDicePlaceholderHtml(this.diceCount)
-
-this.maxHealth = this.health
-
-this.getDiceHtml = function () {
-  this.currentDiceScore = getDiceRollArray(this.diceCount);
+  this.getDiceHtml = function () {
+  this.currentDiceScore = getDiceRollArray(this.diceCount)
   this.diceArray = this.currentDiceScore.map((num) =>
       `<div class="dice">${num}</div>`).join("")
-}
+  }
 
-this.takeDamage = function(attackScoreArray){  
+  this.takeDamage = function (attackScoreArray) {
   const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
   this.health -= totalAttackScore
-  if (this.health <= 0){
-      this.dead = true  
+  if (this.health <= 0) {
+      this.dead = true
       this.health = 0
   }
   }
-  
-  this.HealthBarHtml = function () {
-    const percent = getPercentage(this.health, this.maxHealth)
-    console.log(percent)
+
+  /*
+  CHALLENGE
+  1. Instead of just logging the percent, getHealthBarHtml 
+  needs to return this string of html:
+  `<div class="health-bar-outer">
+  <div class="health-bar-inner *YOUR CODE HERE* " 
+  style="width: *YOUR CODE HERE* %;">
+  </div>
+  </div>`
+  2. You need to make some changes to that string of HTML.
+  2a. If the amount of health left is 25% or lower, add the class 
+  "danger" to the div with the class "health-bar-inner".
+  2b. The width of that div should be the % health remaining. 
+  3. Be sure to add the healthBar variable to the string of HTML
+  rendered by getCharacterHtml.
+  **hint.md for help!!**       
+  */
+
+  this.getHealthBarHtml = function () {
+  const percent = getPercentage(this.health, this.maxHealth)
+
+  return `
+  <div class="health-bar-outer">
+      <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
+      style="width: ${percent}%;">
+      </div>
+  </div>`
   }
 
-this.getCharacterHtml = function () {
-  const { elementId, name, avatar, health, diceCount } = this;
-  const healthBar = this.HealthBarHtml()
+
+  this.getCharacterHtml = function () {
+  const { elementId, name, avatar, health, diceCount } = this
+  const healthBar = this.getHealthBarHtml()
   return `
       <div class="character-card">
           <h4 class="name"> ${name} </h4>
           <img class="avatar" src="${avatar}" />
           <div class="health">health: <b> ${health} </b></div>
+          ${healthBar}
           <div class="dice-container">
               ${this.diceArray}
           </div>
       </div>`
-}
-}
+  }
+  }
 
-export default Character
+  export default Character
