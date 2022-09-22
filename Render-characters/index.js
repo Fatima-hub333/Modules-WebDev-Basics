4a1e04591ef0313e5038a090b38727f8cbf0abb9
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 12:24:54 +0500

    Amalgate the objects

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -4,27 +4,23 @@ function getDiceRollArray(diceCount) {
   })
 }
 
-// function getDiceHtml(diceCount) {
-//   return getDiceRollArray(diceCount).map(function(num) {
-//     return `<div class="dice">${num}</div>`
-//   }).join('')
-// }
-
-const hero = {
-  elementId: 'hero',
-  name:'Wizard' ,
-  avatar:'images/wizard.png' ,
-  health:'60',
-  diceCount: 3
+const characterData = {
+    hero: {
+    elementId: 'hero',
+    name:'Wizard' ,
+    avatar:'images/wizard.png' ,
+    health:'60',
+    diceCount: 3
+  },
+    monster: {
+    elementId:'monster' ,
+    name:'Orc' ,
+    avatar:'images/orc.png' ,
+    health: '10',
+    diceCount: 1
+  }
 }
 
-const monster = {
-  elementId:'monster' ,
-  name:'Orc' ,
-  avatar:'images/orc.png' ,
-  health: '10',
-  diceCount: 1
-}
 
 function Character(data) {
   Object.assign(this, data)
@@ -49,11 +45,12 @@ function Character(data) {
   }
 }
 
-const wizard = new Character(hero)
-const orc = new Character(monster)
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
 
-function() {
+function render() {
   document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
 }
 
+render()
