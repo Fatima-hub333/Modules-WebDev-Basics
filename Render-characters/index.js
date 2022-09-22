817995b8fdd37ca6e8a6230839211b1571f7a0cc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-22 09:34:31 +0500

    object destructuring

--- Render-characters/index.js
+++ Render-characters/index.js
@@ -3,8 +3,10 @@ const hero = {
   name:'Wizard' ,
   avatar:'images/wizard.png' ,
   health:'60' ,
-  diceRoll:6 
+  diceRoll:6
 }
+
+// 
 
 const monster = {
   elementId:'monster' ,
@@ -15,13 +17,14 @@ const monster = {
 }
 
 function renderCharacter(data) {
-  document.getElementById(data.elementId).innerHTML = `
+  const { elementId, name, avatar, health, diceRoll } = data
+  document.getElementById(elementId).innerHTML = `
     <div class="character-card">
-      <h4 class="name"> ${data.name} </h4>
-      <img class="avatar" src="${data.avatar}"/>
-      <p class="health">health: <b> ${data.health} </b></p>
-      <div class="dice-container"><div class="dice"> ${data.diceRoll} </div></div>
-    </div>  
+      <h4 class="name"> ${name} </h4>
+      <img class="avatar" src="${avatar}"/>
+      <p class="health">health: <b> ${health} </b></p>
+      <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+    </div>
   `
 }
 
