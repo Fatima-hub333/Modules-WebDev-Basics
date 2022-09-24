6a12db50280fc8dc48c3dc8b4b5edd7e47bd9eff
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-24 10:59:15 +0500

    fetch data from api & render it

--- bored-api/index.js
+++ bored-api/index.js
@@ -1,5 +1,7 @@
-fetch("https://www.boredapi.com/api/activity")
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("activity").textContent = data.activity
-  })
+document.getElementById("get-activity").addEventListener("click", function() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("activity").textContent = data.activity
+    })
+})
