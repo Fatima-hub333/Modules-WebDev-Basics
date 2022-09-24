4bd75fa1ee1e00a8ec9263c800a210b6abd6b0d3
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-24 12:28:26 +0500

    GET first 5 blog post

--- blogspace/index.js
+++ blogspace/index.js
@@ -1,3 +1,19 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/todos", {method: "GET"})
+/**
+ Challenge:
+ 
+ GET a list of blog posts from the JSON Placeholder API.
+ 
+ BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ Endpoint: /posts
+ 
+ Since there's so many posts, let's limit the array to just 5 items.
+ You can use the `.slice()` array method to just grab the first 5 objects
+ from the data array that comes back from the API
+ */
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    const postsArray = data.slice(0, 5)
+    console.log(postsArray)
+  })
