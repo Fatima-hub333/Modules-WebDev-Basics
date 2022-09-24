ea99aa11163f9fc4cae553cf502bbda208b873ed
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-24 12:36:02 +0500

    BlogSpace- Display blogs on page

--- blogspace/index.js
+++ blogspace/index.js
@@ -15,5 +15,13 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then(response => response.json())
   .then(data => {
     const postsArray = data.slice(0, 5)
-    console.log(postsArray)
+    let html = ""
+    for (let post of postsArray) {
+      html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
+        `
+    }
+    document.getElementById("blog-list").innerHTML = html
   })
