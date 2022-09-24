984181a01cdc166bd79cfa02cc5534ecdbb8dd53
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-24 10:52:38 +0500

    give it styling

--- bored-api/index.js
+++ bored-api/index.js
@@ -1,5 +1,5 @@
 fetch("https://www.boredapi.com/api/activity")
   .then(response => response.json())
-    .then(data => {
-    document.getElementById("activity-name"). textContent = data.activity
+  .then(data => {
+    document.getElementById("activity").textContent = data.activity
   })
