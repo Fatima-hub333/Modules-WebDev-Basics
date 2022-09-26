23693b176b8648c6f9a08082e98c71de6c8a1d2e
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 11:30:54 +0500

    Display our card images

--- DeckGame/index.js
+++ DeckGame/index.js
@@ -14,5 +14,11 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
 document.getElementById("draw-cards").addEventListener("click", () => {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data.cards)
+      document.getElementById("cards").innerHTML = `
+        <img src=${data.cards[0].image} />
+        <img src=${data.cards[1].image} />
+    `
+  })
 })
