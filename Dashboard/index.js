fdfa69243aa7cf18c53d1e8baf5753d6a266218d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-27 00:02:12 +0500

    Get user's current weather

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -43,5 +43,16 @@ time.textContent = date.toLocaleTimeString("en-us", { timeStyle: "short" })
 setInterval(getCurrentTime, 1000)
 
 navigator.geolocation.getCurrentPosition(position => {
-  console.log(position)
+  fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&
+  lon=${position.coords.longitude}&units=imperial`)
+    .then(response => {
+      if (!response.ok) {
+        throw Error("Weather data not available")
+      }
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+  })
 })
+
