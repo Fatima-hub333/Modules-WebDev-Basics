637d6fd6adb48eca6e37b3decf3c5e6d6b49270a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 11:08:33 +0500

    save deckId for later use

--- DeckGame/index.js
+++ DeckGame/index.js
@@ -1,1 +1,12 @@
-//
+let deckId
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      deckId = data.deck_Id
+    })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
