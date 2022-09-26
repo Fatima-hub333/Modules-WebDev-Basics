c76f530dc08916bb7f5792c8170e4c9b6d30649c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 11:22:08 +0500

    draw 2 cards from our deck

--- DeckGame/index.js
+++ DeckGame/index.js
@@ -4,9 +4,15 @@ function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
-      console.log(data)
-      deckId = data.deck_Id
-    })
+        console.log(data)
+        deckId = data.deck_id
+  })
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
