85bbee5e90d09cec2e9f740aec45919d3d08cf56
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 20:38:26 +0500

    promise rejection practice

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -3,6 +3,11 @@ const author = document.getElementById("author")
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(response => response.json())
   .then(data => {
+    console.log(data.urls.regular)
     document.body.style.backgroundImage = `url(${data.urls.regular})`
     author.textContent = `By: ${data.user.name}`
-})
+  })
+
+  .catch(error => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjQyMDYyMDg&ixlib=rb-1.2.1&q=80&w=1080)`
+  })
