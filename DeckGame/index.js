900e2c68d9db6cd89391563128ccbb6c2fe0d4d6
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 09:37:12 +0500

    Callback functions intro

--- DeckGame/index.js
+++ DeckGame/index.js
@@ -1,5 +1,12 @@
-document.getElementById("new-deck").addEventListener("click", function () {
+function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(response => response.json())
     .then(data => console.log(data))
-})
+}
+
+
+
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+// Functions are firs-class object in JavaScript
