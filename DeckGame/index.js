eb0f131cbf92e793927a38d3730d989622648ec2
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 14:34:40 +0500

    Update Index.js

--- DeckGame/index.js
+++ DeckGame/index.js
@@ -11,27 +11,16 @@ const computerScoreEl = document.getElementById("computer-score")
 const myScoreEl = document.getElementById("my-score")
 
 function handleClick() {
-    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-        .then(res => res.json())
-        .then(data => {
-            remainingText.textContent = `Remaining cards: ${data.remaining}`
-            deckId = data.deck_id
-            console.log(deckId)
-        })
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+      remainingText.textContent = `Remaining cards: ${data.remaining}`
+      deckId = data.deck_id
+      console.log(deckId)
+  })
 }
 
 newDeckBtn.addEventListener("click", handleClick)
-
-/**
- * Challenge:
- * 
- * Disable the Draw button when we have no more cards to draw from
- * in the deck.
- * 
- * Disable both the functionality of the button (i.e. change
- * `disabled` to true on the button) AND the styling (i.e. add
- * a `disabled` CSS class to make it look unclickable)
- */
 
 drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
@@ -48,7 +37,14 @@ drawCardBtn.addEventListener("click", () => {
             header.textContent = winnerText
             
             if (data.remaining === 0) {
-                drawCardBtn.disabled = true
+              drawCardBtn.disabled = true
+              if (computerScore > myScore) {
+                header.textContent = "The computer won the game"
+              } else if (myScore > computerScore) {
+                header.textContent = "You won the Game"
+              } else {
+                header.textContent = "It's a tie game!"
+              }
             }
         })
 })
