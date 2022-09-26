56a447f97904e37c02925356302a18c12f2ffe86
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 10:32:20 +0500

    promise concept

--- DeckGame/index.js
+++ DeckGame/index.js
@@ -1,35 +1,12 @@
-// function handleClick() {
-//   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-// }
-
-// document.getElementById("new-deck").addEventListener("click", handleClick)
-
-// function callBack() {
-//   console.log("I finally ran")
-// }
-// setTimeout(callBack, 2000)
-
-
-const people = [
-  { name: "Jack", hasPet: true },
-  { name: "Jill", hasPet: false },
-  { name: "Alice", hasPet: true },
-  { name: "Bob", hasPet: false },
-]
-
-function filterArray(array, callback) {
-  const resultingArray = []
-  for (let item of array){
-    const shouldBeIncluded = callback(item)
-    if (shouldBeIncluded) {
-      resultingArray.push(item)
-    }
-  }
-  return resultingArray
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(response => response.json())
+    .then(data => console.log(data))
 }
 
-const peopleWithPets = filterArray(people, person => person.hasPet)
+document.getElementById("new-deck").addEventListener("click", handleClick)
 
-console.log(peopleWithPets)
+function callBack() {
+  console.log("I finally ran")
+}
+setTimeout(callBack, 2000)
--- DeckGame/reading.md
+++ DeckGame/reading.md
@@ -30,3 +30,27 @@ console.log(peopleOlder)
 //  * Using .filter, create a new array of people who are 18 and older
 //  * (should be Jill, Alice, and Bob)
 //  *
+
+const people = [
+  { name: "Jack", hasPet: true },
+  { name: "Jill", hasPet: false },
+  { name: "Alice", hasPet: true },
+  { name: "Bob", hasPet: false },
+]
+
+function filterArray(array, callback) {
+  const resultingArray = []
+  for (let item of array){
+    const shouldBeIncluded = callback(item)
+    if (shouldBeIncluded) {
+      resultingArray.push(item)
+    }
+  }
+  return resultingArray
+}
+
+const peopleWithPets = filterArray(people, person => person.hasPet)
+
+console.log(peopleWithPets)
+
+<!-- Promises -->
