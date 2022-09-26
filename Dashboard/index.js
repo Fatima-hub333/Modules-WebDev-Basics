2dafdbfa14724c1b8dbbb1c2761ed03ccc750371
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-26 19:53:18 +0500

    Add author info

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -1,5 +1,8 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=unique")
+const author = document.getElementById("author")
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(response => response.json())
   .then(data => {
-    document.body.style.backgroundImage = `url(${data.urls.full})`
-  })
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
+    author.textContent = `By: ${data.user.name}`
+})
