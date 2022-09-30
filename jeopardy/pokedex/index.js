b058ee1b8f1dcd4202bea62cdb96bb0fd567b8ad
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-30 22:13:49 +0500

    pokedex functionality

--- jeopardy/pokedex/index.js
+++ jeopardy/pokedex/index.js
@@ -1,19 +1,45 @@
+/*
+    Pokedex (Challenge)
+    
+    Write and utilize a 
+        displayPokedex(allPokemon) function 
+    
+    Display the Japanese, Chinese, and French
+        name of each Pokemon under its Stats
+        each should take up 1/3rd of the row 
+*/
+
 async function getAllPokemon() {
-  let response = await fetch("pokemon.json")
-  let data = await response.json()
-  return data
+    let response = await fetch("pokemon.json")
+    let data = await response.json()
+    return data.slice(0, 100)
 }
 
-getAllPokemon().then(allPokemon => {
-  let samplePokemon = allPokemon[0]
-  document.body.innerHTML = `<div class="a-pokemon">
-    <div class="a-pokemon-id">${samplePokemon.id}</div>
-    <div class="a-pokemon-name">${samplePokemon.name.english}</div>
-    <div class="a-pokemon-type">${samplePokemon.type.join(' / ')}</div>
+function getPokemonHtml(aPokemon) {
+    return `
+        <div class="a-pokemon">
+            <div class="a-pokemon-id">${aPokemon.id}</div>
+            
+            <div class="a-pokemon-name">${aPokemon.name.english}</div>
+            <div class="a-pokemon-type">${aPokemon.type.join(' / ')}</div>
+            
+            <div class="a-pokemon-stat">HP: ${aPokemon.base.HP}</div>
+            <div class="a-pokemon-stat">Attack: ${aPokemon.base.Attack}</div>
+            <div class="a-pokemon-stat">Defense: ${aPokemon.base.Defense}</div>
+            <div class="a-pokemon-stat">Speed: ${aPokemon.base.Speed}</div>
+            
+            <div class="a-pokemon-alt-name">${aPokemon.name.japanese}</div>
+            <div class="a-pokemon-alt-name">${aPokemon.name.chinese}</div>
+            <div class="a-pokemon-alt-name">${aPokemon.name.french}</div>
+        </div>
+    `
+}
 
-    <div class="a-pokemon-stat">${samplePokemon.base.HP}</div>
-    <div class="a-pokemon-stat">${samplePokemon.base.Attack}</div>
-    <div class="a-pokemon-stat">${samplePokemon.base.Defense}</div>
-    <div class="a-pokemon-stat">${samplePokemon.base.speed}</div>
-  </div>`
-})
+function displayPokedex(allPokemon) {
+    console.log(allPokemon[0])
+    document.body.innerHTML = `<div class="my-pokedex">
+        ${allPokemon.map(getPokemonHtml).join('')}
+    </div>`
+}
+
+getAllPokemon().then(displayPokedex)
